b8a13cd9167cb3c64e11b27d0c9a67aa48f365af
Tim Habersack <tim@hithlonde.com>
2011-11-16 05:19:35 -0800

    Had to set the jquery src to a CDN hosted one

--- jpef_magic.php
+++ jpef_magic.php
@@ -30,7 +30,7 @@ if(mail($to, $subject, $message, $headers))
 ?>
 <html>
 <head>
-<script type="text/javascript" src="jquery-1.6.4.min.js" ></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" ></script>
 <script type="text/javascript">
 $(document).ready(function() {
 	$("#the_form").submit();
